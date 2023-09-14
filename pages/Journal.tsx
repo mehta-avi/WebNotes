b15c1e8e5b08e6d972dfdac2b50582c20ab16831
Avi <avimehta9@gmail.com>
2023-09-13 23:57:37 -0400

    added search frontend, now need backend logic

--- pages/Journal.tsx
+++ pages/Journal.tsx
@@ -2,55 +2,34 @@ import React from 'react';
 import Link from 'next/link';
 
 function Journal() {
+
+   
   return (
-    <body className='bg-black w-screen h-screen items-center px-6 lg:px-32'>
+    <div className='bg-black w-screen h-screen items-center px-6 lg:px-32'>
+        <div className='mx-auto items-center py-4 w-2/4 '>
+            <div className='w-1/2 items-center mx-auto'>
+                <input type='text' placeholder='Search...' className='bg-[#222222] opacity-50 text-white border rounded-md p-2 w-full focus:border-[#FF8B00]'/>
+            </div>
+        </div>
       <div className='max-w-6xl mx-auto py-8 grid gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-7 lg:max-w-screen-xl'>
+       
         {Array.from({ length: new Date().getUTCDate() }, (_, index) => (
           <div
             key={index}
             className='bg-[#222222] rounded-sm w-44 h-40 shadow-md px-1 transition-all duration-300 transform hover:bg-gray-300 hover:scale-105'>
-            {/* Use the Link component to navigate to NotesForDay */}
             <Link
                  href={{
                     pathname: '/NoteForDay',
                     query: {
-                      timestamp: `${new Date().getUTCFullYear()}-${new Date().getUTCMonth() + 1}-${index + 1}`,
+                      timestamp: `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${index + 1}`,
                     }}}>
-              <p className='text-[#F4F5FA] text-right'>{new Date().getUTCMonth() + '/' + (index + 1)} </p>
+              <p className='text-[#F4F5FA] text-right'>{new Date().getMonth() + '/' + (index + 1)} </p>
             </Link>
           </div>
         ))}
       </div>
-    </body>
+    </div>
   );
 }
 
 export default Journal;
-
-
-
-// import { useRouter } from 'next/router';
-// import React from 'react'
-// import Link from 'next/link'
-// import { useUser } from "@auth0/nextjs-auth0/client"
-
-
-// function Journal() {
-//     const { user } = useUser();
-
-//     return (
-//         <body className='bg-[#222222] w-screen h-screen items-center px-6 lg:px-32'>
-//             <div className='max-w-6xl mx-auto py-8 grid gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-7 lg:max-w-screen-xl'>
-//                 {Array.from({ length: new Date().getUTCDate() }, (_, index) => (
-//                     <div key={index} className='bg-[#444444] rounded-sm w-44 h-40 shadow-md px-1 transition-all duration-300 transform hover:bg-gray-300 hover:scale-105'>
-//                         <Link href={`/api/get-note?user_id=${user?.sub}&timestamp=${new Date().getUTCFullYear()}-${new Date().getUTCMonth() + 1}-${index + 1}`}>
-//                             <p className='text-right'>{new Date().getUTCMonth() + "/" + (index+1)} </p>
-//                         </Link>
-//                     </div>
-//                 ))}
-//             </div>
-//         </body>
-//     )
-// }
-
-// export default Journal
--- pages/LandingPage.tsx
+++ pages/LandingPage.tsx
@@ -2,7 +2,6 @@ import { useRouter } from 'next/router';
 import React from 'react'
 
 function LandingPage() {
-
   const router = useRouter(); // Import the useRouter hook
 
   const handleSubmit = () => {
@@ -10,12 +9,12 @@ function LandingPage() {
   }
 
   return (
-    <body className="bg-black h-screen w-screen flex justify-center items-center">
+    <div className="bg-black h-screen w-screen flex justify-center items-center">
       <div className='text-center p-8 rounded-lg'>
         <h1 className="text-[#FF8B00] text-4xl mb-4">Welcome to your Private Journal on the Internet</h1>
         <button className="bg-[#222222] text-[#FF8B00] mt-8 px-4 py-2 rounded-md" onClick={handleSubmit}>Login</button>
       </div>
-    </body>
+    </div>
   )
 }
 
